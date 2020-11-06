a7cfc91b68093fdd36b7daa8be549967c9a18904
Danila Verstov <VerstovDI@yandex.ru>
2020-11-06 13:53:21 +0300

    Добавьте файлы проекта.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ReportingSubsystem.Areas.Identity.Data;
+using ReportingSubsystem.Data;
+
+[assembly: HostingStartup(typeof(ReportingSubsystem.Areas.Identity.IdentityHostingStartup))]
+namespace ReportingSubsystem.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
